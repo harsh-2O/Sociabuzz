0e506bc90a2fe8223cc4424ccbc6133cac5d25cb
harsh-2O <202051117@iiitvadodara.ac.in>
2023-04-06 13:40:54 +0530

    Added App.js

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,13 @@
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+// import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
+
+function App() {
+  return <Home/>
+  // return <Profile/>
+  // return <Register/>
+  // return <Login/>
+}
+
+export default App;
